8ac659dbce98fb08dbd7e09b9b872461e962f85d
Collin Holmquist <collinholmquist@gmail.com>
2020-09-11 09:32:36 -0500

    Create ForLoopsExercises.java

---
+++ ForLoopsExercises.java
@@ -0,0 +1,34 @@
+public class ForLoopExercises {
+
+    public static void main(String [] args) {
+    
+      //Call each method to test
+      
+      ExerciseOne();
+      //ExerciseTwo();
+      //ExerciseThree();
+      //ExerciseFour();
+    
+    }
+    
+    //Exercise 1
+    public static void ExerciseOne() {
+    
+    }
+    
+    //Exercise 2
+    public static void ExerciseTwo() {
+    
+    }
+    
+     //Exercise 3
+    public static void ExerciseThree() {
+    
+    }
+    
+     //Exercise 4
+    public static void ExerciseFour() {
+    
+    }
+
+}
